d0083c41c1ec894b1f00cd700c05b6f97fa60386
wangyijun <985695123@qq.com>
2018-05-21 23:57:13 +0800

    Bean与xml映射

--- test/Demo.java
+++ test/Demo.java
@@ -4,6 +4,7 @@ import org.dom4j.io.SAXReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,10 @@ public class Demo {
         List<Book> bookList = new ArrayList<Book>();
         for (Element e : elementList){
             Book book = new Book();
+            for (Element a : e.elements()){
+                System.out.println(a.getName());
+            }
+            System.out.println(e.getName());
             book.setTitle(e.elementText("title"));
             book.setAuthor(e.elementText("author"));
             book.setYear(e.elementText("year"));
@@ -32,5 +37,41 @@ public class Demo {
 
 
     }
+    public  List<?> xmlmap(Class<?> cls) throws Exception {
+        SAXReader sr = new SAXReader();
+        Document document = sr.read(new File("./test/book.xml"));
+        Element root = document.getRootElement();
+        List<Element> elements = root.elements();
+        List<Object> objectList = new ArrayList<Object>();
 
+        for (Element e : elements) {
+            Object obj = cls.newInstance();
+
+            // Object object = new Object();
+            for (Element a : e.elements()) {
+                Field field = cls.getDeclaredField(a.getName());
+                field.setAccessible(true);
+                field.set(obj, a.getText());
+            }
+            objectList.add(obj);
+        }
+
+        return objectList;
+    }
+
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        try {
+            List<Book> books = (List<Book>)demo.xmlmap(Book.class);
+            for (Book book : books) {
+                System.out.println("title:" + book.getTitle() + "\t category:" + book.getCategory() + "\t author:" + book.getAuthor() + "\t year:" + book.getYear() + "\t price:" + book.getPrice());
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
